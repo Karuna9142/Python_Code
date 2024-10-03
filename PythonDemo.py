70b68e5a93aa7d3ba00701eab421849fe2e75d4d
Karuna Kumari <mishrakaruna9142@gmail.com>
2024-10-03 10:40:22 +0530

    Add files via upload

---
+++ PythonDemo.py
@@ -0,0 +1,15 @@
+print("hello world")
+name = "Karuna"
+age = 22
+print("Name:- ",name," \n" "Age:- ",age)
+
+#Printing variable and literals
+x = 5
+y = 8
+
+print("X = ",x, "Y = ",y)
+print("Sum:- ",x+y)
+
+#printing with formatting
+percent = 86.22
+print("Score: {:.2f}%".format(percent))
