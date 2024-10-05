347d153e58d3479b7b5c8e26496441f79ce1262f
Karuna Kumari <mishrakaruna9142@gmail.com>
2024-10-05 16:18:52 +0530

    Add files via upload

---
+++ DictionariesEx.py
@@ -0,0 +1,11 @@
+#key-value pair
+#{}
+#mutable
+
+person = {'name': 'Karuna', 'Age':25, 'City':'Noida'}
+print('Person:- ',person)
+print(person['name'])
+
+#modify dictionaries
+person['age'] = 27
+print("person:- ",person)
