98765664b5ebe1a0719150e99d47bf19b9159f13
Karuna Kumari <mishrakaruna9142@gmail.com>
2024-10-05 16:18:36 +0530

    Add files via upload

---
+++ SetsEx.py
@@ -0,0 +1,11 @@
+#unorderd
+#mutable
+#do not contain duplicate elements
+# use {}
+
+set = {1,2,3,4,5}
+print("Sets:- ",set)
+
+#create another set
+set2 = {"abc","pqr","lmn","xyz"}
+print("Set2:- ",set2)
