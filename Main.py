c39347db96593897014bdcf2bd003c7ca13b8b11
Karuna Kumari <mishrakaruna9142@gmail.com>
2024-10-05 16:17:06 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,17 @@
+print("Hello World" "\n" "Welcome to python");
+print(20+30);
+print('20+30')
+print(10<5)
+a = 10
+b = 10
+
+# if varibale value will be same then i will store the value in the same location
+# id is used to check the value
+print(id(a),id(b))
+
+# variable name should not be start with a number, @
+
+a = "Hello"
+b  = " World"
+
+print(a+b)
