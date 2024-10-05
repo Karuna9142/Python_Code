a19c5e9ebbdcf8ebdc678548c053935c99587e99
Karuna Kumari <mishrakaruna9142@gmail.com>
2024-10-05 16:16:51 +0530

    Add files via upload

---
+++ OperatorEx.py
@@ -0,0 +1,65 @@
+a = 10
+b = 20
+
+# Arithmetic Operator
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a//b)
+
+# Assignment Operator
+print(a==b)
+a = a+5;
+print("now value of a is",a)
+
+b = b-10
+print("Value of b is",b)
+
+#Comparison Operator
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a<=b)
+print(a>=b)
+
+#Logical Operator
+#and
+print(a<5 and b<10 )
+
+#or
+print(a<20 or b<10);
+print(not a==b)
+
+#Membership Operator
+# in
+#not in
+
+str = "HEllo"
+print('h' in str)
+print('h' not in str)
+
+num = [10,20,30,40]
+print(10 in num)
+print(10 not in num)
+
+# Identity Opera..
+k = 10
+l = 10
+
+print(k is l, k==l)
+print(k is not l, k!=l)
+
+#BitWise Operator
+x1 = 10
+y1 = 8
+ # bin is used to find binary number
+print(bin(x1))
+print(bin(y1))
+
+print(x1&y1) #8
+print(x1|y1) #10
+print(x1^y1)    #2
+
