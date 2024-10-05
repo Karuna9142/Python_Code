91a639abf2f5738c7a62eb2d2e5cc1e217a4d4ce
Karuna Kumari <mishrakaruna9142@gmail.com>
2024-10-05 16:18:12 +0530

    Add files via upload

---
+++ TupleEx.py
@@ -0,0 +1,11 @@
+# ordered Collection
+#immutable
+# use open brackets()
+
+num = (5,6)
+x,y = num
+print("X:- ",x, "Y:- ",y)
+
+#Create another tuple
+t = ("cabbage", "Potato","Tomato","Pea")
+print(t)
