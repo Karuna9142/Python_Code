44f6b4bda9cd9a1683116ea2c69bd236380dd7e6
Karuna Kumari <mishrakaruna9142@gmail.com>
2024-10-06 18:01:04 +0530

    Add files via upload

---
+++ UserInputEx.py
@@ -0,0 +1,19 @@
+# user input
+#Integer
+'''
+a = int(input("Enter the value:- "))
+print(a)
+
+b = int(input("Enter the value:- "))
+print(b)
+
+sum = a+b;
+print("Sum of ",a, "and",b, "is:- ",sum)
+'''
+#Eval - can store any type of data
+# it can also store the binary number
+x = eval(input("Enter the first number"))
+y = eval(input("Enter the second number"))
+
+print(x+y)
+
