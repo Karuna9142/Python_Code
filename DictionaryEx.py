e2f9897c20b5f9f80c3d5fb2bbd47aa3e698f1fa
Karuna Kumari <mishrakaruna9142@gmail.com>
2024-10-06 18:00:26 +0530

    Add files via upload

---
+++ DictionaryEx.py
@@ -0,0 +1,13 @@
+# unordered key value pair
+#mutable
+
+d = {'Name':'Karuna',"Gender":'Female', 'Age':22}
+print(d,type(d))
+
+print(d['Gender'])
+
+
+#Set - Immutable, {},unique element
+
+s = {10,20,30,40,50,30}
+print(s,type(s))
