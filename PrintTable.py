b21440daffdc1d6c469836ca9156b4648a669756
Karuna Kumari <mishrakaruna9142@gmail.com>
2024-10-07 23:55:26 +0530

    Add files via upload

---
+++ PrintTable.py
@@ -0,0 +1,14 @@
+'''
+n = int(input("Enter a number:- "))
+
+for i in range(1,11):
+    print(n, "x",i, "= " ,n*i)
+
+'''
+#range(10,0,-1)
+
+for n in range(10, 0, -1):
+    print(n)
+print()
+for n in range(10,-1,-2):
+    print(n)
