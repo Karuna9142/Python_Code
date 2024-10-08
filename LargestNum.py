1d4eadd39d56aa8b600531704618fae43e9f087b
Karuna Kumari <mishrakaruna9142@gmail.com>
2024-10-08 23:56:20 +0530

    Add files via upload

---
+++ LargestNum.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a:- "))
+b = int(input("Enter b:- "))
+c = int(input("Enter c:- "))
+
+if(a>b and a>c):
+    print("a is Largest number")
+if b>a and b>c :
+    print("b is Largest number")
+if c>a and  c>a:
+    print("c is Largest number")
