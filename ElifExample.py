d3f401b01637342344eca2d908a56b11da3422c8
Karuna Kumari <mishrakaruna9142@gmail.com>
2024-10-08 23:45:03 +0530

    Add files via upload

---
+++ ElifExample.py
@@ -0,0 +1,10 @@
+number = int(input("Enter the number?"))
+
+if number==10:
+    print("equal to 10")
+elif number==50:
+    print("equal to 50")
+elif number == 100:
+    print("equal to 100")
+else:
+    print("not equal to 10,50,100")
