8d0d36293cb2b05fac1eab88297fb92e38e3e745
Karuna Kumari <mishrakaruna9142@gmail.com>
2025-05-21 23:10:07 +0530

    Add files via upload

--- TupleEx.py
+++ TupleEx.py
@@ -1,11 +1,7 @@
-# ordered Collection
-#immutable
-# use open brackets()
+t = (5, 'karuna', 1+3j)
+print(t, type(t))
+# in tuple there should be more than 1 value
+# if there is one valuein tuple then i will consider as integer
 
-num = (5,6)
-x,y = num
-print("X:- ",x, "Y:- ",y)
-
-#Create another tuple
-t = ("cabbage", "Potato","Tomato","Pea")
-print(t)
+t = (10)
+print(t, type(t))
