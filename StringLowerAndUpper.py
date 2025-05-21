5e95ccb9093a4b5d6ca35ebb2828dbb0b3f95451
Karuna Kumari <mishrakaruna9142@gmail.com>
2025-05-21 23:13:44 +0530

    Add files via upload

---
+++ StringLowerAndUpper.py
@@ -0,0 +1,13 @@
+str = "Karuna Mishra"
+inLowerCase = str.lower()
+inUpperCase = str.upper()
+print(inLowerCase) #lowercase
+print(inUpperCase) #uppercase
+
+#Mix - upper and lower
+t = str.title()
+print(t)
+
+#Capitalize - only fst letter will be capital
+c = str.capitalize()
+print(c)
