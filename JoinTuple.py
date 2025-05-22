f8d032b0ac7041fd8075eb272c22ab8ab6b59c7b
Karuna Kumari <mishrakaruna9142@gmail.com>
2025-05-22 16:59:07 +0530

    Add files via upload

---
+++ JoinTuple.py
@@ -0,0 +1,14 @@
+# t1 = ('a','b','c')
+# t2 = (1,2,3)
+# t3 = t1+t2
+# print(t3)
+#
+# mytuple = t1*2
+# print(mytuple)
+
+t = (1,2,3,4,1,2,2,3);
+
+c = t.count(2)
+ind = t.index(4)
+print(c)
+print(ind)
---
+++ ListComprehensive.py
@@ -0,0 +1,7 @@
+l = []
+for a in range(1,101):
+    l.append(a)
+
+print(l)
+n = [m for m in range(1, 101) if m%2==1]
+print(n)
