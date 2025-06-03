fefbb2a16f5beabc693a78cce152cc199c474333
Karuna Kumari <mishrakaruna9142@gmail.com>
2025-06-03 22:51:23 +0530

    Add files via upload

---
+++ Square.py
@@ -0,0 +1,5 @@
+def square(x):
+    return x*x
+
+s = square(5)
+print(s)
---
+++ SumFun.py
@@ -0,0 +1,6 @@
+def sum(a,b=5):
+    print(a+b)
+
+n = int(input("Enter a number"))
+m = int(input("Enter a second number"))
+sum(n,m)
