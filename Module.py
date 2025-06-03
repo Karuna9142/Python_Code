f47976c2743efd0009067c6b0ce62375f64f9fdc
Karuna Kumari <mishrakaruna9142@gmail.com>
2025-06-03 22:54:11 +0530

    Add files via upload

---
+++ Module.py
@@ -0,0 +1,17 @@
+#first way
+# import Module1
+#
+# print(Module1.sum(4,6))
+# print(Module1.mul(2,9))
+
+#second way
+# from Module1 import *
+#
+# s = sum(2,5)
+# print(s)
+#
+# print(mul(4,8))
+
+#third way
+import Module1 as m
+print(m.sum(9,5))
